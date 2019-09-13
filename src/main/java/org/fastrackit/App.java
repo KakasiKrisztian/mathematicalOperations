b1200487a259a22e8751267e35428bc105d3c66e
Krisztian Kakasi <cristy11_1998@yahoo.com>
2019-09-13 11:45:43 +0300

    First commit with 4 simple math operations

---
+++ src/main/java/org/fastrackit/App.java
@@ -0,0 +1,28 @@
+package org.fastrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String args[]) {
+
+
+        MathOperations mathOperations = new MathOperations();
+
+        mathOperations.sum(2 , 6);
+
+        mathOperations.difference(973.4, 824.9);
+
+        mathOperations.multiplication(243, 845);
+
+        mathOperations.divide(53, 13);
+
+
+    }
+
+
+
+
+}
---
+++ src/main/java/org/fastrackit/MathOperations.java
@@ -0,0 +1,32 @@
+package org.fastrackit;
+
+public class MathOperations {
+
+    String name;
+
+
+    public double sum(double x, double y) {
+        double actualSum = x + y;
+        System.out.println("The result for the sum of " + x + " & " + y + " is: " + actualSum);
+        return actualSum;
+    }
+
+    public double difference(double x, double y) {
+        double actualDifference = x - y;
+        System.out.println("The result for the difference between " + x + " & " + y + " is: " + actualDifference);
+        return actualDifference;
+    }
+
+    public int multiplication(int x, int y) {
+        int actualMultiplication = x * y;
+        System.out.println("The result for the multiplication between " + x + " & " + y + " is: " + actualMultiplication);
+        return actualMultiplication;
+    }
+
+    public double divide(double x, double y){
+        double actualDivision = x / y;
+        System.out.println("The result we get from dividing " + x + " by " + y + " is: " + actualDivision);
+        return actualDivision;
+
+    }
+}
